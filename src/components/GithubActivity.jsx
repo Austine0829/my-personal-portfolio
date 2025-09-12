79be079e9c34af8d262955f12e78b4c1b0d6f8ee
Austine0829 <audreytuazon29@gmail.com>
2025-09-12 21:31:16 +0800

    (add) github act compo

---
+++ src/components/GithubActivity.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+
+function GithubActivity() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchGitHubData = async () => {
+      const query = `
+        {
+          user(login: "Austine0829") {
+            publicRepos: repositories(privacy: PUBLIC) {
+              totalCount
+            }
+            latestRepo: repositories(first: 1, orderBy: {field: PUSHED_AT, direction: DESC}) {
+              nodes {
+                name
+                pushedAt
+                url
+                defaultBranchRef {
+                  name
+                  target {
+                    ... on Commit {
+                      messageHeadline
+                      committedDate
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `;
+
+      const response = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+        },
+        body: JSON.stringify({ query }),
+      });
+
+      const json = await response.json();
+      console.log("🔍 Full API response:", JSON.stringify(json, null, 2));
+      setData(json.data.user);
+    };
+
+    fetchGitHubData();
+  }, []);
+
+  const repoCount = data.publicRepos?.totalCount ?? 0;
+  const latestRepo = data.latestRepo?.nodes?.[0];
+  const lastCommit = latestRepo?.defaultBranchRef?.target;
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center overflow-x-hidden border h-full p-5 rounded-lg">
+        <h1 className="text-[clamp(.2rem,2.5vw,1rem)]">
+          Github | {new Date(lastCommit.committedDate).toDateString()}
+        </h1>
+        <p className="text-[clamp(.8rem,3vw,1rem)] text-center font-bold">
+          {latestRepo.name}
+        </p>
+        <h2 className="text-[clamp(.6rem,2.5vw,1rem)] text-center" >
+          {lastCommit.messageHeadline}
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default GithubActivity
