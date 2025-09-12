326ae75a32d2a8bd372aa7cc8f9c3a26b5fcb97b
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 00:26:03 +0800

    (update) padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <div className="border p-1 rounded-lg">
+      <div className="border p-2 rounded-lg">
         <span className="text-[clamp(.8rem,3vw,1rem)] font-bold">Philippines</span>
       </div>
     </>);
