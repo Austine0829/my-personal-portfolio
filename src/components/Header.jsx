efc40babf8cac96477c296770f5931c28a5ba7e1
Austine0829 <audreytuazon29@gmail.com>
2025-09-12 20:01:53 +0800

    [add] 2 new components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <div className="border p-1 rounded-lg">
+        <span className="text-xs font-bold">Philippines</span>
+      </div>
+    </>);
+}
+
+export default Header
