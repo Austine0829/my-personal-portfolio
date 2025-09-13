40ae10623810ee286562895d69c0ca2aceb8433a
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 13:15:01 +0800

    (update) recent act compo

--- src/components/RecentActivities.jsx
+++ src/components/RecentActivities.jsx
@@ -6,12 +6,12 @@ function RecentActivities() {
     <>
       <div className="grid grid-cols-12 grid-rows-6 gap-2 rounded-lg">
         <div className="flex border justify-center p-3 col-span-12 row-span-2 rounded-lg">
-          <h1 className="text-[clamp(.8rem,3vw,1rem)] font-bold">Recent Activities</h1>
+          <h1 className="text-[13px] font-bold">Recent Activities</h1>
         </div>
-        <div className="col-span-6 row-span-5">
+        <div className="col-span-6 row-span-5 md:col-span-12">
           <GithubActivity />
         </div>
-        <div className="col-span-6 row-span-5">
+        <div className="col-span-6 row-span-5 md:col-span-12">
           <LastDotFm />
         </div>
       </div>
