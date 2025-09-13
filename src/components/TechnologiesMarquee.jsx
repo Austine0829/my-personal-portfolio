04fc4d7911cf9cae6440bb1f8aea7a926d42043b
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 22:15:15 +0800

    (update) padding

--- src/components/TechnologiesMarquee.jsx
+++ src/components/TechnologiesMarquee.jsx
@@ -5,7 +5,7 @@ function TechlogiesMarquee() {
     <>
       <div className="border border-gray-300 bg-white p-1 rounded-lg">
         <Marquee speed={50}>
-          <div className=" bg-gray-200 p-1 rounded-sm">
+          <div className=" bg-gray-200 p-2 rounded-sm">
             <i className="si si-github me-1"></i>
             <span className="text-[13px] font-bold">Github</span>
           </div>
