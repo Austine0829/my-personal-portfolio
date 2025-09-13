ca160ce89f9bf4e3632585f30737bd7b8b3c42f0
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 10:52:24 +0800

    (remove) fixed height

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ function Profile() {
   return (
     <>
       <div className="flex flex-col gap-5 border p-5 rounded-lg">
-        <img className="border w-full h-70 rounded-2xl"
+        <img className="border w-full rounded-2xl"
           src="/portfolio_profile.jpg" />
         <p className="text-[clamp(.5rem,3vw,1rem)]">
           heya! <span className="bg-black text-white">i'm Tuazon, Austine Audrey</span> a Filipino Software Engineer in the making and a 2nd Year BS Information Technology student at Pampanga State University.
