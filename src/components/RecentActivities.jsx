e3ec7a52eb48f1295343d0b112b83980dae0d50c
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 18:51:57 +0800

    (add) bg n border color

--- src/components/RecentActivities.jsx
+++ src/components/RecentActivities.jsx
@@ -5,7 +5,7 @@ function RecentActivities() {
   return (
     <>
       <div className="grid grid-cols-12 grid-rows-6 gap-1 rounded-lg md:grid-rows-12">
-        <div className="flex border justify-center items-center p-3 col-span-12 row-span-2 rounded-lg">
+        <div className="flex border border-gray-300 bg-white justify-center items-center p-3 col-span-12 row-span-2 rounded-lg">
           <h1 className="text-[13px] font-bold">Recent Activities</h1>
         </div>
         <div className="col-span-6 row-span-5 md:col-span-12">
