ce281e799618a07ea092f9138b1d8f229b4cb91e
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 11:08:22 +0800

    (add) loc icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import { MapPinIcon } from '@heroicons/react/24/outline'
+
 function Header() {
   return (
     <>
-      <div className="border p-2 rounded-lg">
-        <span className="text-[clamp(.8rem,3vw,1rem)] font-bold">Philippines</span>
+      <div className="flex items-center border p-2 rounded-lg">
+        <div className='animate-bounce'>
+          <MapPinIcon className='w-5' />
+        </div>
+        <span className="text-[clamp(.8rem,3vw,1rem)] font-bold">
+          Philippines
+        </span>
       </div>
     </>);
 }
