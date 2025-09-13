0cea5854f3d545b541968edf9876e7533ac51586
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 12:50:39 +0800

    (add) ghub stats

---
+++ src/components/GithubStats.jsx
@@ -0,0 +1,19 @@
+import GithubRepoAndContri from "./GithubRepoAndContri";
+import GithubCalendar from "./GithubCalendar";
+
+function GithubStats() {
+  return (
+    <>
+      <div className="grid grid-cols-12 grid-rows-2 gap-2">
+        <div className="col-span-12 row-span-4 gap-2">
+          <GithubRepoAndContri />
+        </div>
+        <div className="col-span-12 row-span-8">
+          <GithubCalendar />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GithubStats
