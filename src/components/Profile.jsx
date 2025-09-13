d85661821e5d351e0220118a9b6506f7ce93f93e
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 18:47:11 +0800

    (add) bg/border color

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 function Profile() {
   return (
     <>
-      <div className="flex flex-col gap-5 border p-5 rounded-lg md:flex-row md:h-full">
-        <img className="border w-full rounded-2xl md:w-96"
+      <div className="flex flex-col gap-5 border border-gray-300 bg-white p-5 rounded-lg md:flex-row md:h-full">
+        <img className="w-full rounded-2xl md:w-96"
           src="/portfolio_profile.jpg" />
         <p className="text-[10px]">
           heya! <span className="bg-black text-white">i'm Tuazon, Austine Audrey</span> a Filipino Software Engineer in the making and a 2nd Year BS Information Technology student at Pampanga State University.
