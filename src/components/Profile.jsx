beb0a50064e8eaf70c93f4fcd627c0fb5a00997e
Austine0829 <audreytuazon29@gmail.com>
2025-09-13 13:13:45 +0800

    (update) prof compo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 function Profile() {
   return (
     <>
-      <div className="flex flex-col gap-5 border p-5 rounded-lg">
-        <img className="border w-full rounded-2xl"
+      <div className="flex flex-col gap-5 border p-5 rounded-lg md:flex-row md:h-full">
+        <img className="border w-full rounded-2xl md:w-96"
           src="/portfolio_profile.jpg" />
-        <p className="text-[clamp(.5rem,3vw,1rem)]">
+        <p className="text-[10px]">
           heya! <span className="bg-black text-white">i'm Tuazon, Austine Audrey</span> a Filipino Software Engineer in the making and a 2nd Year BS Information Technology student at Pampanga State University.
         </p>
       </div>
