92829bfe8de5a067c40ac93798d10f5519c5fd89
Austine0829 <audreytuazon29@gmail.com>
2025-09-14 22:15:38 +0800

    fix layout responsivenes and add project panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Profile from './components/Profile'
 import Header from "./components/Header"
 import RecentActivities from './components/RecentActivities'
 import TechlogiesMarquee from './components/TechnologiesMarquee'
 import GithubStats from './components/GithubStats'
 import InterestMarquee from './components/InteresetMarquee'
+import Projects from './components/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='grid grid-cols-12 gap-2 m-4'>
-        <div className='grid grid-cols-12 gap-1 p-1 col-span-12 md:pb-7 md:max-h-screen md:col-span-7 md:overflow-y-auto no-scrollbar'>
-          <div className='col-span-12'>
+      <div className="lg:absolute lg:inset-0 lg:flex lg:items-center lg:justify-center">
+        <div className="grid grid-cols-12 gap-4 h-full w-full p-4 lg:flex lg:max-h-[768px] lg:max-w-[1920px]">
+          <div className='flex flex-col gap-1 col-span-12 overflow-y-auto no-scrollbar'>
             <Header />
-          </div>
-          <div className='col-span-12 xl:col-span-7'>
             <Profile />
-          </div>
-          <div className='col-span-12 xl:col-span-5'>
-            <RecentActivities />
-          </div>
-          <div className='col-span-12'>
             <TechlogiesMarquee />
-          </div>
-          <div className='col-span-12'>
             <GithubStats />
-          </div>
-          <div className='col-span-12'>
             <InterestMarquee />
           </div>
-        </div>
-
-        <div className='border p-1 col-span-12 md:col-span-5'>
-          <h1>Hello <i className='si si-github'></i></h1>
+          <div className='flex flex-col gap-1 h-full w-full col-span-12'>
+            <div className='flex gap-1 border border-gray-300 bg-white p-1 rounded-lg'>
+              <button className='bg-gray-200 p-2 px-3 text-[13px] font-bold rounded-lg'>Projects</button>
+              <button className='bg-gray-200 p-2 px-3 text-[13px] font-bold rounded-lg'>Connect</button>
+            </div>
+            <div className='flex flex-col h-screen overflow-y-auto w-fit no-scrollbar '>
+              <Projects />
+            </div>
+          </div>
         </div>
       </div>
     </>
