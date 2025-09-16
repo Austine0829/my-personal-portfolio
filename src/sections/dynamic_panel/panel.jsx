592705b48b055cc3cb6511cc6970417a2abab9ce
Austine0829 <audreytuazon29@gmail.com>
2025-09-16 22:44:35 +0800

    (add) connect

--- src/sections/dynamic_panel/panel.jsx
+++ src/sections/dynamic_panel/panel.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import Projects from "../../components/project/Projects";
-import { PROJECTS } from "../../constants/constants";
+import { CONNECT, PROJECTS } from "../../constants/constants";
+import Connect from "../../components/connect/Connect"
+
+function NavigationButton({ label, isActive, onClick }) {
+	return (
+		<>
+			<button className={`${isActive ? 'bg-black text-white' : 'bg-gray-200'} p-2 px-3 text-[13px] font-bold rounded-lg cursor-pointer`}
+				onClick={onClick}>{label}</button>
+		</>
+	);
+}
 
 function Panel() {
 	const [pageName, setPageName] = useState(PROJECTS);
@@ -8,11 +18,16 @@ function Panel() {
 	return (
 		<>
 			<div className='flex gap-1 border border-gray-300 bg-white p-1 rounded-lg'>
-				<button className='bg-gray-200 p-2 px-3 text-[13px] font-bold rounded-lg'>Projects</button>
-				<button className='bg-gray-200 p-2 px-3 text-[13px] font-bold rounded-lg'>Connect</button>
+				<NavigationButton label={PROJECTS}
+					isActive={pageName === PROJECTS}
+					onClick={() => { setPageName(PROJECTS) }} />
+				<NavigationButton label={CONNECT}
+					isActive={pageName === CONNECT}
+					onClick={() => { setPageName(CONNECT) }} />
 			</div>
-			<div className='flex flex-col h-screen overflow-y-auto w-fit no-scrollbar '>
+			<div className='flex flex-col overflow-y-auto no-scrollbar '>
 				{pageName === PROJECTS && <Projects />}
+				{pageName === CONNECT && <Connect />}
 			</div>
 		</>);
 }
