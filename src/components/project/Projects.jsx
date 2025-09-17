bc0195c65d506686ed1f227a540aae7e524be8d5
Austine0829 <audreytuazon29@gmail.com>
2025-09-18 00:49:55 +0800

    (add) height constraint

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -21,7 +21,7 @@ function Projects() {
 
   return (
     <>
-      <div className="flex flex-col gap-1 bg-gray-200 p-1 rounded-lg">
+      <div className="flex flex-col gap-1 bg-gray-200 p-1 rounded-lg max-h-[768px]">
         {
           projects.map(project => (
             <Project data={project} />
