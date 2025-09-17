196c1e4ba5e1372eae62854f2a38901d65af2163
Austine0829 <audreytuazon29@gmail.com>
2025-09-17 23:18:36 +0800

    (add) interest service

--- src/sections/interest/InteresetMarquee.jsx
+++ src/sections/interest/InteresetMarquee.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
+import asyncGetInterest from "../../services/interestService";
 
 function InterestMarquee() {
+  const [interests, setInterests] = useState([]);
+
+  useEffect(() => {
+    async function fetchInterests() {
+      try {
+        const data = await asyncGetInterest();
+        setInterests(data);
+      } catch (error) {
+
+      }
+    }
+
+    fetchInterests();
+  }, []);
+
   return (
     <>
       <div className="grid grid-cols-12 gap-1">
@@ -8,11 +25,13 @@ function InterestMarquee() {
           <h1 className="text-[13px] font-bold">Interest</h1>
         </div>
         <div className="border col-span-8 border-gray-300 bg-white p-1 rounded-lg xl:col-span-10">
-          <Marquee>
-            <div className="bg-gray-200 p-5 rounded-lg">
-              <span className="text-[10px]">Interest</span>
-              <h1 className="text-[13px] font-bold">Web Development</h1>
-            </div>
+          <Marquee speed={30}>
+            {interests.map((interest, index) => (
+              <div key={index} className=" bg-gray-200 p-5 mx-0.5 rounded-lg">
+                <span className="text-[10px]">Interest</span>
+                <h1 className="text-[13px] font-bold">{interest.name}</h1>
+              </div>
+            ))}
           </Marquee>
         </div>
       </div>
