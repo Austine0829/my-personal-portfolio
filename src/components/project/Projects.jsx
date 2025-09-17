b0b70f846e02488d16e6d796749334e84aa78b0c
Austine0829 <audreytuazon29@gmail.com>
2025-09-17 23:18:49 +0800

    (add) projects service

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import Project from "./project";
+import asyncGetProjects from "../../services/projectsService"
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const data = await asyncGetProjects();
+
+        setProjects(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    fetchProjects();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col gap-1 bg-gray-200 p-1 rounded-lg">
-        <Project />
-        <Project />
-        <Project />
+        {
+          projects.map(project => (
+            <Project data={project} />
+          ))
+        }
       </div>
     </>);
 }
