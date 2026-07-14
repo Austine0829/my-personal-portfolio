cc8834d3a38dda987e379d4b5dabd11d5768556e
Austine0829 <audreytuazon29@gmail.com>
2026-07-15 00:15:32 +0800

    update description

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -4,18 +4,18 @@ function Profile() {
   return (
     <>
       <div className="grid grid-cols-12 gap-1">
-        <div className="flex flex-col gap-5 col-span-12 border border-gray-300 bg-white p-5 rounded-lg md:flex-row md:h-full xl:col-span-7">
+        <div className="flex flex-col gap-5 col-span-12 border border-gray-300 bg-white p-5 rounded-lg md:flex-row md:h-full">
           <img className="w-full rounded-2xl md:w-96"
             src="/portfolio_profile.jpg" />
           <p className="text-[10px]">
-            A 23-year-old Filipino, <span className="bg-black text-white">Software Engineer in the making</span>,
-            and currently a second-year Bachelor of Science in
-            Information Technology student at Pampanga State University (formerly DHVSU). Driven by curiosity, I
-            have explored various programming languages, frameworks, and tools to
-            solve problems, and automate processes.
+            A 23-year-old Filipino, <span className="bg-black text-white">Programmer in the making</span>,
+            and currently a third-year Bachelor of Science in
+            Information Technology student at Pampanga State University (formerly DHVSU). As a curiosity-driven programmer, 
+            I have explored various programming languages, frameworks, and tools to solve problems, automate processes, 
+            and build systems that I have an interest with.
           </p>
         </div>
-        <div className="col-span-12 xl:col-span-5">
+        <div className="col-span-12">
           <RecentActivities />
         </div>
       </div>
