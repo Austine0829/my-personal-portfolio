ed6b226092dec3ed58530d24afeae9bd879b78ab
Austine0829 <audreytuazon29@gmail.com>
2026-07-15 14:23:57 +0800

    add new button labels based on category

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,4 +1,6 @@
-import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { DocumentArrowDownIcon, DocumentTextIcon } from "@heroicons/react/24/solid";
+import { EyeIcon } from "@heroicons/react/24/solid";
+import { GlobeAltIcon } from "@heroicons/react/24/solid";
 
 function Project({ data }) {
     return (
@@ -17,16 +19,48 @@ function Project({ data }) {
                             ))
                         }
                     </div>
-                    {
-                        data.url != '' && <div className="mt-1">
-                            <a className="flex gap-1 w-fit bg-black p-2 border text-[11px] font-bold text-white rounded-lg cursor-pointer"
-                                href={data.url}
-                                target="_blank">
-                                <GlobeAltIcon className="w-4" />
-                                Visit Website
-                            </a>
-                        </div>
-                    }
+                    <div className="flex gap-1">
+                        {
+                            data.repoUrl != '' && <div className="mt-1">
+                                <a className="flex gap-1 w-fit bg-black p-2 border text-[11px] font-bold text-white rounded-lg cursor-pointer"
+                                    href={data.repoUrl}
+                                    target="_blank">
+                                    <EyeIcon className="w-4" />
+                                    View Repository
+                                </a>
+                            </div>
+                        }
+                        {
+                            
+                            data.url != "" && <div className="mt-1">
+                                <a className="flex w-fit bg-black p-2 border text-[11px] font-bold text-white rounded-lg cursor-pointer"
+                                    href={data.url}
+                                    target="_blank">
+                                        {
+                                            data.category == "website" && 
+                                            <div className="flex gap-1">
+                                                <GlobeAltIcon className="w-4" />
+                                                Visit Website
+                                            </div>    
+                                        }
+                                        {
+                                            data.category == "mobile" && 
+                                            <div className="flex gap-1">
+                                                <DocumentArrowDownIcon className="w-4" />
+                                                See Release
+                                            </div>    
+                                        }
+                                        {
+                                            data.category == "model" && 
+                                            <div className="flex gap-1">
+                                                <DocumentTextIcon className="w-4" />
+                                                See Process
+                                            </div>    
+                                        }
+                                </a>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
         </>
